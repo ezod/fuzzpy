abad41557c4244c1b89e571ea3d7ff14c3783d72
Aaron Mavrinac <aaron@wintermute.(none)>
2009-03-26 15:05:05 -0400

    Add __init__.py for fuzz module.

---
+++ fuzz/__init__.py
@@ -0,0 +1,11 @@
+"""\
+FuzzPy: Fuzzy sets for Python
+
+@author: Aaron Mavrinac
+@organization: University of Windsor
+@contact: mavrin1@uwindsor.ca
+@license: GPL-3
+"""
+
+__import__('pkg_resources').declare_namespace(__name__)
+__version__ = (0, 0, 0)
