85b4b0e821ddef74b68afedf600b9e5b0cacf170
Aaron Mavrinac <mavrinac@gmail.com>
2009-04-14 20:52:11 -0400

    Add crisp graph class.

--- fuzz/fgraph.py
+++ fuzz/fgraph.py
@@ -26,6 +26,64 @@ class GraphEdge( object ):
         self.head = head
 
 
+class Graph( object ):
+    """\
+    Crisp graph class (used for alpha cuts and crisp methods).
+    """
+    def __init__( self, viter = None, eiter = None, directed = True ):
+        """\
+        Construct a crisp graph from optional iterables.
+
+        @param viter: The iterable for the vertex set (optional).
+        @type viter: C{object}
+        @param eiter: The iterable for the edge set (optional).
+        @type eiter: C{object}
+        @param directed: Defines the graph as directed or undirected.
+        @type directed: C{bool}
+        """
+        self._V = set( viter )
+        if eiter is not None:
+            for edge in eiter:
+                if not isinstance( edge, GraphEdge ):
+                    raise TypeError, ( "Edge set must consist of GraphEdges" )
+                elif not edge.tail in self.vertices \
+                or not edge.head in self.vertices:
+                    raise KeyError, ( "Tail and head must be in vertex set" )
+        self._E = set( eiter )
+        self.directed = directed
+
+    @property
+    def vertices( self ):
+        """\
+        Return a set of vertices in the graph.
+
+        @return: A set of vertices.
+        @rtype: C{set}
+        """
+        return self._V
+
+    def edges( self, tail = None, head = None ):
+        """\
+        Return a set of edges with tail and/or head optionally specified.
+
+        @param tail: The tail vertex constraint (optional).
+        @type tail: C{object}
+        @param head: The head vertex constraint (optional).
+        @type head: C{object}
+        @return: The fuzzy set of edges specified.
+        @rtype: C{set}
+        """
+        result = set()
+        if ( tail is not None and not tail in self._V ) \
+        or ( head is not None and not head in self._V ):
+            raise KeyError, ( "Specified tail/head must be in vertex set" )
+        for edge in self._E:
+            if ( tail is None or edge.tail == tail ) \
+            and ( head is None or edge.head == head ):
+                result.add( edge )
+        return result
+
+
 class FuzzyGraph( object ):
     """\
     Fuzzy graph class.
@@ -55,10 +113,10 @@ class FuzzyGraph( object ):
     @property
     def vertices( self ):
         """\
-        Return a list of vertices in the fuzzy graph. Returns
+        Return a set of vertices in the fuzzy graph.
 
-        @return: A list of vertices.
-        @rtype: C{list}
+        @return: A set of vertices.
+        @rtype: C{set}
         """
         return self._V.objects
 
