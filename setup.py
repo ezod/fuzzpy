97d6906158a50b98882053c7e11707a0fe8722ad
Aaron Mavrinac <mavrinac@gmail.com>
2009-05-07 14:46:29 -0400

    Update setup.py for better PyPI inclusion.

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ version = "%s.%s.%s" % __version__[0:3]
 from setuptools import setup
 
 setup(
-    name = "fuzz",
+    name = "FuzzPy",
     version = version,
     license = "GPL",
-    description = "FuzzPy",
-    long_description = "Library for fuzzy sets, fuzzy graphs, and general fuzzy mathematics for Python.",
+    description = "Library for fuzzy sets, fuzzy graphs, and general fuzzy mathematics for Python.",
     author = "Aaron Mavrinac",
     author_email = "mavrin1@uwindsor.ca",
     url = "http://code.google.com/p/fuzzpy",
+    download_url = "http://fuzzpy.googlecode.com/files/fuzzpy-0.0.1.tar.bz2",
     keywords = "fuzzy set graph math",
     packages = ['fuzz'],
     zip_safe = False,
