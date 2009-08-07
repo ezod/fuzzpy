1a96f3485ef25a6b7026c20d21cc6422b50c890d
Aaron Mavrinac <mavrinac@gmail.com>
2009-08-07 13:16:23 -0400

    Update download URL in setup.py.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author = "Aaron Mavrinac",
     author_email = "mavrin1@uwindsor.ca",
     url = "http://code.google.com/p/fuzzpy",
-    download_url = "http://fuzzpy.googlecode.com/files/fuzzpy-0.0.1.tar.bz2",
+    download_url = "http://fuzzpy.googlecode.com/files/fuzzpy-0.0.2.tar.bz2",
     keywords = "fuzzy set graph math",
     packages = ['fuzz'],
     zip_safe = False,
