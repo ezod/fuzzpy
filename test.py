3920c566ae2e2dd036b655345fa71f88d93ab30e
Aaron Mavrinac <aaron@pygar.(none)>
2009-09-29 22:07:46 -0400

    More FuzzyGraph tests.

--- test.py
+++ test.py
@@ -15,10 +15,10 @@ import fuzz
 print "FuzzPy imported from '%s'" % fuzz.__path__[ 0 ]
 
 
-class TestFuzzyGraphShortestPaths( unittest.TestCase ):
+class TestFuzzyGraph( unittest.TestCase ):
     
     def setUp( self ):
-        v = [ 1, 2, 3, 4, 5 ]
+        v = set( [ 1, 2, 3, 4, 5 ] )
         self.D = fuzz.FuzzyGraph( viter = v, directed = True )
         self.U = fuzz.FuzzyGraph( viter = v, directed = False )
         for G in [ self.U, self.D ]:
@@ -28,6 +28,46 @@ class TestFuzzyGraphShortestPaths( unittest.TestCase ):
             G.connect( 4, 5, 0.7 )
             G.connect( 3, 5, 0.2 )
             G.connect( 5, 2, 0.5 )
+            G.connect( 1, 5, 0.0 )
+
+    def test_vertices( self ):
+        exp = set( [ 1, 2, 3, 4, 5 ] )
+        act = self.U.vertices
+        self.assertEqual( act, exp )
+
+    def test_mu( self ):
+        exp = [ 0., 0.7, 0.7, 0. ]
+        act = [ self.U.mu( 1, 3 ),
+                self.U.mu( 4, 5 ),
+                self.U.mu( 5, 4 ),
+                self.D.mu( 5, 4 ) ]
+        self.assertEqual( act, exp )
+
+    def test_weight( self ):
+        exp = [ 0., 1., float( 'inf' ), 1. / 0.9, 1. / 0.9, float( 'inf' ) ]
+        act = [ self.U.weight( 1, 1 ),
+                self.U.weight( 2, 3 ),
+                self.U.weight( 1, 5 ),
+                self.D.weight( 3, 4 ),
+                self.U.weight( 4, 3 ),
+                self.D.weight( 4, 3 ) ]
+        self.assertEqual( act, exp )
+
+    def test_adjacent( self ):
+        act = True
+        act &= self.D.adjacent( 1, 2 )
+        act &= not self.D.adjacent( 2, 1 )
+        act &= self.U.adjacent( 1, 2 )
+        act &= self.U.adjacent( 2, 1 )
+        self.assert_( act )
+
+    def test_connected( self ):
+        act = True
+        act &= self.D.connected( 1, 5 )
+        act &= not self.D.connected( 5, 1 )
+        act &= self.U.connected( 1, 5 )
+        act &= self.U.connected( 5, 1 )
+        self.assert_( act )
 
     def test_dijkstra_directed( self ):
         exp = { 1 : None, 2 : 1, 3 : 2, 4 : 3, 5 : 4 }
