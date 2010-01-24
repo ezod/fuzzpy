0a3a2fbf877449d77d4df7c771e8763590dd934d
Aaron Mavrinac <mavrinac@gmail.com>
2010-01-24 15:48:00 -0500

    Add basic trapezoidal fuzzy number class.

--- fuzz/fnumber.py
+++ fuzz/fnumber.py
@@ -12,4 +12,40 @@ class FuzzyNumber( object ):
     Fuzzy number class.
     """
     def __init__( self ):
-        raise NotImplementedError, ( "Fuzzy numbers are not yet implemented" )
+        raise NotImplementedError, ( "please use one of the subclasses" )
+
+
+class TrapezoidalFuzzyNumber( FuzzyNumber ):
+    """\
+    Trapezoidal fuzzy number class.
+    """
+    def __init__( self, kernel, support ):
+        """\
+        Constructor.
+
+        @param kernel: The kernel of the fuzzy number.
+        @type kernel: C{tuple} of C{float}
+        @param support: The support of the fuzzy number.
+        @type support: C{tuple} of C{float}
+        """
+        if support[ 0 ] > kernel[ 0 ] or support[ 1 ] < kernel[ 1 ]:
+            raise ValueError, ( "kernel range must be within support range" )
+        self.kernel = kernel
+        self.support = support
+
+    def mu( self, x ):
+        """\
+        Return the membership degree of a point in the universal set domain of
+        the fuzzy number.
+
+        @param x: A point in the universal set.
+        @type x: C{float}
+        """
+        if x >= self.kernel[ 0 ] and x <= self.kernel[ 1 ]:
+            return 1.
+        elif x > self.support[ 0 ] and x < self.kernel[ 0 ]:
+            return ( x - self.support[ 0 ] ) / ( self.kernel[ 0 ] - self.support[ 0 ] )
+        elif x < self.support[ 1 ] and x > self.kernel[ 1 ]:
+            return ( self.support[ 1 ] - x ) / ( self.support[ 1 ] - self.kernel[ 1 ] )
+        else:
+            return 0.
