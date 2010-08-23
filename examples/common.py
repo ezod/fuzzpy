b8925760226425d49c2baf1a65ab937295bd415b
Xavier Spriet <linkadmin@gmail.com>
2010-08-23 00:18:33 -0400

    Use real path for fuzz import hack

--- examples/common.py
+++ examples/common.py
@@ -6,10 +6,11 @@ Common initialization code for examples.
 @contact: mavrin1@uwindsor.ca
 @license: GPL-3
 """
-
+import os
+import os.path
 import sys
 
-sys.path.insert(0, '..')
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__))))
 
 import fuzz
 print "FuzzPy imported from '%s'\n" % fuzz.__path__[0]
