36b30ff3999532da91f554f86fdc72c18a3c2d3f
Aaron Mavrinac <mavrinac@gmail.com>
2010-08-24 14:54:53 -0400

    Add Gnuplot visualization plugin for fuzzy numbers, and improve the PyDot plugin.

--- fuzz/vis_plugins/graph_pydot.py
+++ fuzz/vis_plugins/graph_pydot.py
@@ -1,15 +1,17 @@
-import pydot
+"""\
+Pydot visualization plugin for FuzzPy.
+
+@author: Xavier Spriet
+@contact: linkadmin@gmail.com
+@license: LGPL-3
+"""
+
 from ..fgraph import FuzzyGraph
 from ..graph import Graph
 from abc_plugin import AbstractPlugin
 
-# Class Name
 VIS_PLUGIN = 'FuzzPyDot'
-
-# Supported Visualization Datatypes
 VIS_TYPES = [FuzzyGraph, Graph]
-
-# Supported Output Formats
 VIS_FORMATS = ['bmp', 'canon', 'dot', 'xdot', 'eps', 'fig', 'gd', 'gd2', 
     'gif', 'gtk', 'ico', 'imap', 'cmapx', 'jpg', 'jpeg', 'jpe', 'pdf', 
     'plain', 'plain-ext', 'png', 'ps', 'ps2', 'svg', 'svgz', 'tif', 'tiff', 
@@ -23,88 +25,82 @@ class FuzzPyDot(AbstractPlugin):
     This plugin converts vertices and edges to pydot elements then create
     a rendering based on the 'format' attribute (default=ps).
     """
-    
-    # Graph instance
-    _G = None
-    # Graph name
-    name = 'Fuzzpy Graph'
-    
     def __init__(self, obj=None, *args, **kwargs):
         """\
-        Plugin Constructor
+        Plugin constructor.
         
         Maps the specified graph object locally and marshalls method
         keywords to local arguments for the PyDot object.
         
-        @param obj: Graph, FuzzyGraph or graph instance
-        @type obj: L{Graph} or L{FuzzyGraph} or C{graph}
-        @return: Initialized plugin instance
-        @rtype: VisPlugin
+        @param obj: The graph or fuzzy graph to visualize.
+        @type obj: L{Graph} or L{FuzzyGraph}
         """
+        import pydot
+        self.pydot = pydot
         self._G = obj
         
         if ('name' in kwargs.keys()):
             self.name = kwargs['name']
+        else:
+            name = 'FuzzPy Graph'
     
     @staticmethod
     def is_supported():
         """\
-        Returns True if we can import our dependencies, False otherwise.
-        
-        @rtype: Boolean
+        Return whether this plugin is supported.
+
         @return: True if the plugin can run in this environment.
+        @rtype: C{bool}
         """
         try:
             import pydot
-        except ImportError, ex:
-            warning.warn("PyDot plugin will not run on this system. \
-            You must install the PyDot python module first.")
+        except ImportError:
+            warning.warn(("PyDot plugin will not run on this system. "
+                          "You must install the PyDot Python module first."))
             return False
         return True
     
     def marshall_vertices(self):
         """\
-        Converts vertices to PyDot nodes
+        Converts vertices to PyDot nodes.
         
         Adds GraphViz attributes to each vertex and converts it into a
         pydot.Node object.
         
-        @rtype: list
-        @return: list of pydot.Node objects
+        @return: List of pydot.Node objects.
+        @rtype: C{list}
         """
         vertices = []
         for vertex in self._G.vertices():
             if isinstance(self._G, FuzzyGraph):
-                node = pydot.Node(
+                node = self.pydot.Node(
                     name="g_%s" % str(vertex),
                     label=str(vertex),
                     weight=str(self._G.mu(vertex)),
                     penwidth="%.2f" % self._G.mu(vertex)
                 )
             elif isinstance(self._G, Graph):
-                node = pydot.Node(
+                node = self.pydot.Node(
                     name="g_%s" % str(vertex),
                     label=str(vertex),
                 )
             vertices.append(node)
-            
         return vertices
         
     def marshall_edges(self):
         """\
-        Converts edges to PyDot edges
+        Converts edges to PyDot edges.
         
         Adds GraphViz attributes to edges and registers them all as PyDot
         edge objects.
         
-        @rtype: list
-        @returns: list of pydot.Edge objects
+        @returns: List of pydot.Edge objects.
+        @rtype: C{list}
         """
         edges = []
-        
         for edge in self._G.edges():
             if isinstance(self._G, FuzzyGraph):
-                connector = pydot.Edge(
+                connector = self.pydot.Edge(
                     src="g_%s" % str(edge.head), 
                     dst="g_%s" % str(edge.tail),
                     weight=str(self._G.mu(edge.tail, edge.head)),
@@ -115,30 +111,24 @@ class FuzzPyDot(AbstractPlugin):
                         edge.head) + 0.05) * 2.0)
                 )
             elif isinstance(self._G, Graph):
-                connector = pydot.Edge(
+                connector = self.pydot.Edge(
                     "g_%s" % str(edge.head), 
                     "g_%s" % str(edge.tail),
                 )
             edges.append(connector)
-            
         return edges
-        
     
     def visualize(self, *args, **kwargs):
         """\
-        PyDot visualization method
+        PyDot visualization method.
         
         Converts all vertices and edges from the graph provided to the
         plugin's constructor into a PyDot graph and converts any apppropriate
         attributes to be passed to GraphViz.
         
-        This method will return a postscript string that can be written into
-        a file, or passwd through stdin to a postscript viewer such as gv.
-        
-        @return: Postscript visualization string
+        @return: Output data string.
         @rtype: C{str}
         """
-        
         # Pick default output format if required
         if kwargs.has_key('format') and kwargs['format'] in VIS_FORMATS:
             output_format = kwargs['format']
@@ -150,15 +140,15 @@ class FuzzPyDot(AbstractPlugin):
         else:
             gtype = 'graph'
         
-        D = pydot.Dot(rankdir='rt', graph_type=gtype)
-        
+        D = self.pydot.Dot(rankdir='rt', graph_type=gtype)
+
         # Convert vertices and edges to PyDot nodes/connectors
         for vertex in self.marshall_vertices():
             D.add_node(vertex)
         
         for edge in self.marshall_edges():
             D.add_edge(edge)
-        
+
         # Return formatted output
         return (output_format, D.create(format=output_format))
 
--- fuzz/vis_plugins/num_gnuplot.py
+++ fuzz/vis_plugins/num_gnuplot.py
@@ -1,1 +1,80 @@
-types = ['FuzzyNumber']
+"""\
+Gnuplot-py visualization plugin for FuzzPy.
+
+@author: Aaron Mavrinac
+@organization: University of Windsor
+@contact: mavrin1@uwindsor.ca
+@license: LGPL-3
+"""
+
+import os
+import tempfile
+from time import sleep
+
+from ..fnumber import PolygonalFuzzyNumber
+from abc_plugin import AbstractPlugin
+
+VIS_PLUGIN = 'FuzzPyGnuplot'
+VIS_TYPES = [PolygonalFuzzyNumber]
+VIS_FORMATS = ['png']
+
+
+class FuzzPyGnuplot(AbstractPlugin):
+    """\
+    Gnuplot-py visualization plugin for FuzzPy.
+    """
+    def __init__(self, obj=None, *args, **kwargs):
+        """\
+        Plugin constructor.
+
+        @param obj: The fuzzy number to visualize.
+        @type obj: L{PolygonalFuzzyNumber}
+        """
+        from Gnuplot import Gnuplot
+        self.Gnuplot = Gnuplot
+        self._N = obj
+
+    @staticmethod
+    def is_supported():
+        """\
+        Return whether this plugin is supported.
+
+        @return: True if the plugin can run in this environment.
+        @rtype: C{bool}
+        """
+        try:
+            import Gnuplot
+        except ImportError:
+            warning.warn(("Gnuplot plugin will not run on this system. "
+                          "You must install Gnuplot-py first."))
+            return False
+        return True
+
+    def visualize(self, *args, **kwargs):
+        """\
+        Gnuplot-py visualization mehod.
+
+        @return: Postscript visualization string.
+        @rtype: C{str}
+        """
+        if kwargs.has_key('format') and kwargs['format'] in VIS_FORMATS:
+            output_format = kwargs['format']
+        else:
+            output_format = 'png'
+        plot = self.Gnuplot()
+        plot('set terminal %s' % output_format)
+        plot('set data style lines')
+        tmpdir = tempfile.mkdtemp()
+        filename = os.path.join(tmpdir, 'gnuplot-output')
+        plot('set output \"%s\"' % filename)
+        plot.plot([[p[0], p[1]] for p in self._N.points])
+        # FIXME: is there a better way to know when Gnuplot output is ready?
+        sleep(0.5)
+        tmpfile = open(filename, 'r')
+        output_data = tmpfile.read()
+        tmpfile.close()
+        os.remove(filename)
+        os.rmdir(tmpdir)
+        return (output_format, output_data)
+
+AbstractPlugin.register(FuzzPyGnuplot)
